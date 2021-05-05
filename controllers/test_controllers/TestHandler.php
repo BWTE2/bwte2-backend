03803f1c25c9ebd7df25ecbbbefe896052b3f966
matejPeluha <matej.peluha@gmail.com>
2021-05-05 22:13:37 +0200

    ADD: adding test withou questions

--- controllers/test_controllers/TestHandler.php
+++ controllers/test_controllers/TestHandler.php
@@ -8,12 +8,25 @@ class TestHandler extends DatabaseCommunicator
     public function __construct()
     {
         //ODKOMENTOVAT PRI POUZIVANI DATABAZY A ZAPISAT UDAJE O DATABAZE DO config.php
-        //parent::__construct();
+        parent::__construct();
     }
 
     public function addTest($data, $key){
-        //TODO: pridat test
+        $this->addEmptyTest($data, $key);
         return ["result" => "created"];
+    }
+
+    private function addEmptyTest($data, $key){
+        /*
+         *  KYM NIE JE UROBENE PRIHLASOVANIE UCITELA, VYTVORTE SI V DATABAZE NEJAKEHO UCITELA S ID 1
+         */
+        $_SESSION["lecturerId"] = 1;
+
+
+        $teacherId = $_SESSION["lecturerId"];
+        $query = "INSERT INTO test(teacher_id, title, code, is_active, duration) VALUES (?,?,?,?,?)";
+        $bindParameters = [$teacherId, $data->name, $key, 0, $data->timeLimit];
+        $this->pushToDatabase($query, $bindParameters);
     }
 
     public function activateTest($key){
