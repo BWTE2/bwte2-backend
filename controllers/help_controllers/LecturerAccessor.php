040e98d96f013a360c2635ac8ac8f34397eb6b5e
matejPeluha <matej.peluha@gmail.com>
2021-05-13 17:48:07 +0200

    nemam energiu na vymyslanie commitov

--- controllers/help_controllers/LecturerAccessor.php
+++ controllers/help_controllers/LecturerAccessor.php
@@ -8,7 +8,7 @@ class LecturerAccessor extends DatabaseCommunicator
     public function __construct()
     {
         //ODKOMENTOVAT PRI POUZIVANI DATABAZY A ZAPISAT UDAJE O DATABAZE DO config.php
-        //parent::__construct();
+        parent::__construct();
     }
 
     public function handleRegistrationEvent($data){
@@ -29,5 +29,17 @@ class LecturerAccessor extends DatabaseCommunicator
     public function isRegistered($data){
         //TODO: ak bude treba
         return true;
+    }
+
+    public function getLecturerInfo($id){
+        $query = "SELECT id, name, surname, email FROM teacher WHERE id=:id";
+        $bindParameters = [":id" => $id];
+        $results = $this->getFromDatabase($query, $bindParameters);
+
+        if(empty($results)){
+            return ["id" => null, "name" => null, "surname" => null, "email" => null];
+        }
+
+        return $results[0];
     }
 }
